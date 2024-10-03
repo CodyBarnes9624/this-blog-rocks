c00648a0a3a36cafbb375738f7962295775682ac
Cody Barnes <codybarnes9624@gmail.com>
2024-10-03 19:40:56 -0400

    modified starter code, added components, pages, app.jsx and Index.jsx

---
+++ client/src/components/BlogList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ posts }) => {
+  return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <Link to={`/blog/${post.id}`}>Read More</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
