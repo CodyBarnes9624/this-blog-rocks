f7949665e1ad9fbf77fc4328e338f9e4b741f6b6
Cody Barnes <codybarnes9624@gmail.com>
2024-10-17 22:14:59 -0400

    added dist

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ const startServer = async () => {
     app.use('/api/blog', blogRoutes); 
 
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/build')));
+        app.use(express.static(path.join(__dirname, '../client/dist')));
         app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
         });
     }
 
